779f770e519282d9c9b899761bf910a947b2f9d8
kritik <kritikverma2002@gmail.com>
2022-04-05 02:00:30 +0530

    optimized

--- code/Easy/Arrays/Merge Sorted Array.cpp
+++ code/Easy/Arrays/Merge Sorted Array.cpp
@@ -6,6 +6,33 @@
 
 using namespace std;
 
+void merge__Optimized(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    int last = m + n - 1;
+
+    while (n > 0 && m > 0)
+    {
+        if (nums1[m - 1] > nums2[n - 1])
+        {
+            nums1[last] = nums1[m - 1];
+            m--;
+        }
+        else
+        {
+            nums1[last] = nums2[n - 1];
+            n--;
+        }
+        last--;
+    }
+
+    while (n > 0)
+    {
+        nums1[last] = nums2[n - 1];
+        n--;
+        last--;
+    }
+}
+
 vector<int> merge(vector<int> nums1, int m, vector<int> nums2, int n)
 {
     if (n == 0)
